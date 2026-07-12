08ced0a80123922e476bc780bac37e7bb8122a7d
sujalpatel0510 <sujalpatel0510@gmail.com>
2026-07-12 14:07:34 +0530

    routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,14 @@
 from flask import Blueprint
 from .health import health_bp
 from .social_bp import social_bp
+from .environmental_bp import environmental_bp
+from .governance_bp import governance_bp
+from .gamification_bp import gamification_bp
+
 
 def register_blueprints(app):
     app.register_blueprint(health_bp, url_prefix='/api')
     app.register_blueprint(social_bp, url_prefix='/api/social')
+    app.register_blueprint(environmental_bp, url_prefix='/api/environmental')
+    app.register_blueprint(governance_bp, url_prefix='/api/governance')
+    app.register_blueprint(gamification_bp, url_prefix='/api/gamification')
